3d20af77a9cc0ba457586edf2e827d79f33859cf
famcodings <numanfarukh.work@gmail.com>
2023-08-02 03:18:16 +0500

    add delete place route and controller

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -8,5 +8,6 @@ router.get("/:pid", placesController.getPlaceById)
 router.get("/user/:uid", placesController.getPlaceByUserId)
 router.post("/", placesController.createPlace)
 router.patch("/:pid", placesController.updatePlace)
+router.delete("/:pid", placesController.deletePlace)
 
 module.exports = router
